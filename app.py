50a9c3bc1b950845657d1138588fa46ec3d67bc4
ramithaabeyratne <ramithaa@99x.io>
2023-06-07 12:39:21 +0530

    'yaml'

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Python xyz1 - test X - Demo Time - Flask app running!'
+    return 'Python xyz1 - test XX - Demo Time - Flask app running!'
